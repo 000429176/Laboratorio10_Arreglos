2d509598bf9af4a0a0de022bc12effdd71831fec
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-04-12 22:16:17 -0500

    Solución 9.3.1

--- Laboratorio10.py
+++ Laboratorio10.py
@@ -4,17 +4,21 @@ Created on Fri Apr  3 10:30:11 2020
 
 @author: Omar
 """
+
+import numpy as np
+
+
 #La función realiza la potencia de b de un numero a 
 
 def a_power_b(a,b):
     contP = 0
     ContPar = 0
     ContImp = 0
-    while a!=0 :    
+ 
         
 #En caso de que b sea negativo la potencia sería 1/a**b, por tanto se utiliza el condicional b<0 para identificar si b es negativo. La variable c representa la potencia normal y la variable d el resultado final
         
-        if b<0 :
+    if b<0 :
             c = 1
             b = b*-1
             for i in range(1, b+1):
@@ -23,27 +27,32 @@ def a_power_b(a,b):
             
 #Si no se cumple ninguna de las condiciones ya planteadas, simplemente se realiza la ptencia de a**b. Utilizando una variable c para el calculo de la potencia y usando un ciclo for para multiclar a un numero de b veces
         
-        else: 
+    else: 
             c = 1
             for i in range(1, b+1):
                 c = (c)*a
-        contP += 1
+    contP += 1
         
-        if c%2==0 :
+    if c%2==0 :
             ContPar += 1
-        else:
+    else:
             ContImp += 1
         
-        print("El resultado de " + str(a) + " elevado a la " + str(b) + " es: " + str(c))
+    print("El resultado de " + str(a) + " elevado a la " + str(b) + " es: " + str(c))
         
-        a = int(input("Ingrese a: "))
-        b = int(input("Ingrese b: "))
         
     print("Se calculron " + str(contP) + " potencias, " + str(ContPar) + " fueron pares " + " y " + str(ContImp) + " fueron impares")
 
 #----------------------------------Principal----------------------------------#
 
-a = int(input("Ingrese a: "))
-b = int(input("Ingrese b: "))
+n = int(input("Ingrese el numero de parejas, mínimo 5: "))
 
-a_power_b(a,b)
+while n<5 :
+    n = int(input("Ingrese el numero de parejas, mínimo 5: "))
+
+a = np.zeros([n])
+b = np.zeros([n])
+for i in range(0,n):
+	a[i]=int(input("ingrese a: "))
+	b[i]=int(input("ingrese b: "))
+
